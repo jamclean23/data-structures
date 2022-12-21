34e70be0092913ae880087b36ed2f8689ba0657c
jamclean23 <jamclean23@gmail.com>
2022-12-21 13:51:16 -0500

    Add comment structure for testing, Add objective comments at the top

---
+++ linked-list/linked-list.js
@@ -0,0 +1,63 @@
+// Linked List Project
+
+/*Assignment
+
+OBJECTIVE:
+    - Build a module method that will convert an array into a linked list.
+    - Build functionality so that the module passes a series of tests.
+
+You will need two classes or factories:
+
+    LinkedList class / factory, which will represent the full list.
+    Node class / factory, containing a value function and a link to the nextNode,
+        set both as null by default.
+
+Build the following functions in your linked list class:
+
+    append(value) adds a new node containing value to the end of the list
+    prepend(value) adds a new node containing value to the start of the list
+    size returns the total number of nodes in the list
+    head returns the first node in the list
+    tail returns the last node in the list
+    at(index) returns the node at the given index
+    pop removes the last element from the list
+    contains(value) returns true if the passed in value is in the list and otherwise returns false.
+    find(value) returns the index of the node containing value, or null if not found.
+    toString represents your LinkedList objects as strings, so you can print them out and preview them
+        in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+
+Extra Credit
+    insertAt(value, index) that inserts a new node with the provided value at the given index.
+    removeAt(index) that removes the node at the given index.
+    Extra Credit Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their nextNode link updated.
+    */
+
+
+// TESTS
+
+// Add a node containing a value to the end of the list with DataManager.append(value)
+
+// Add a node containing a value to the beginning of the list with DataManager.prepend(value)
+
+// DataManager.size() returns the total number of nodes
+
+// DataManager.head() returns the first node on the list
+
+// DataManager.tail() returns the last node on the list
+
+// DataManager.at(index) returns the node at the given index
+
+// DataManager.pop() removes the last element from the list
+
+// DataManager.contains(value) returns true if the given value is in the list, false if not
+
+// DataManager.find(value) returns the index of the value if it's in the list, or null if it is not
+
+// DataManager.toString() returns the entire list in string format
+
+// DataManager.insertAt(value, index) inserts a new node at provided index with provided value
+
+// DataManager.removeAt(index) removes the node at given index
+
+
+
